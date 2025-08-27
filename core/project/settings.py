229d070d9ba4d18052b2b6a2299b65a60047a1dd
Nazire Mathe <154861651+sphiwe-mathe5@users.noreply.github.com>
2025-08-27 23:34:39 +0200

    Update settings.py

--- core/project/settings.py
+++ core/project/settings.py
@@ -237,20 +237,3 @@ CHANNEL_LAYERS = {
     },
 }
 
-# -----------------------------
-# Logging
-# -----------------------------
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'root': {
-        'handlers': ['console'],
-        'level': 'WARNING',
-    },
-}
-
