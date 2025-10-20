937cde50b649d8dcb6683706f48e9e7bf6abe4b6
sphiwe-mathe5 <mathesphiwe689@gmail.com>
2025-10-20 22:24:28 +0200

    DB

--- core/project/settings.py
+++ core/project/settings.py
@@ -124,16 +124,16 @@ TEMPLATES = [
 ]
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': config('DATABASE_ENGINE'),
-        'NAME': config('DATABASE_NAME'),
-        'USER': config('DATABASE_USER'),
-        'PASSWORD': config('DATABASE_PASSWORD'),
-        'HOST': config('DATABASE_HOST'),
-        'PORT': config('DATABASE_PORT'),
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': config('DATABASE_ENGINE'),
+#        'NAME': config('DATABASE_NAME'),
+#        'USER': config('DATABASE_USER'),
+#        'PASSWORD': config('DATABASE_PASSWORD'),
+#        'HOST': config('DATABASE_HOST'),
+#        'PORT': config('DATABASE_PORT'),
+#    }
+#}
 
 CACHES = {
     "default": {
